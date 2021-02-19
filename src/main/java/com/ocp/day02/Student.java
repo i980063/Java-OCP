65ba8e1dc555ad5ca6afd4b2ae83a80b26f444cb
user <user@LAPTOP-QR26C5JR>
2021-02-19 21:38:16 +0800

    Java OCP

--- src/main/java/com/ocp/day02/Student.java
+++ src/main/java/com/ocp/day02/Student.java
@@ -1,7 +1,7 @@
 package com.ocp.day02;
 //data class
 public class Student {
-    //屬性
+    //屬性/變數/資產/欄位
     //public String schoolName;//學校
     public static String schoolName;//學校，變成Global(全域)
     public String name;//姓名
