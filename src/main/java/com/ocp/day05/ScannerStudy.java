bde0f0a9564a2c5895213a22d3c4fb379c465a10
user <user@LAPTOP-QR26C5JR>
2021-02-26 19:01:25 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day05/ScannerStudy.java
@@ -0,0 +1,13 @@
+package com.ocp.day05;
+//預設會import java.lang.*;(舉凡System、Math...)
+import java.util.Scanner;
+//import java.util.*;(util底下的全部，但不符合企業使用原則)
+
+public class ScannerStudy {
+    public static void main(String[] args) {
+        //java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        
+    }
+  
+}
