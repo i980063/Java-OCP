7f9ab9c3f75431fd588258745e1447a2e93efcbd
user <user@LAPTOP-QR26C5JR>
2021-02-26 20:35:20 +0800

    Java OCP

--- src/main/java/com/ocp/day05/ArrayDemo2.java
+++ src/main/java/com/ocp/day05/ArrayDemo2.java
@@ -8,16 +8,24 @@ public class ArrayDemo2 {
         int[] scores1 = {80,90,90,100,40,50};
         System.out.printf("數學:%s\n",Arrays.toString(scores1));
         
+        //總分
         //int sum = scores1[0]+scores1[1]+scores1[2]+scores1[3]+scores1[4]+scores1[5];
+        //JAVA7 for-loop
         int sum = 0;
         
-        //for(int i = 0;i < scores1.length ;i++){
+        //for(int i = 0;i < scores1.length ;i++)
         for(int i = 0, lens = scores1.length ;i < lens ;i++){
             sum += scores1[i];
         }
         
         System.out.printf("總分: %d\n",sum);
         
+        //JAVA for-in(for-each)
+        int sum2 = 0;
+        for(int x:scores1){
+            sum2 += x;
+        }
+        System.out.printf("總分: %d\n",sum2);
         
     }
     
