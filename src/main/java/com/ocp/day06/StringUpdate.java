d4a3a29c5c9be243cde3f605e361587c8147322b
user <user@LAPTOP-QR26C5JR>
2021-03-05 20:33:18 +0800

    Java OCP

--- src/main/java/com/ocp/day06/StringUpdate.java
+++ src/main/java/com/ocp/day06/StringUpdate.java
@@ -5,6 +5,11 @@ public class StringUpdate {
        x = x.concat("8");
     }
     
+   public static String updateString2(String x) {
+       x = x.concat("8");
+       return x;//回傳位置 Pass by Value
+    }   
+    
     public static void updateStringBuilder(StringBuilder y) {//(StringBuilder y)區域變數
         //y = y.append("8");
         y.append("8");//與上面答案一樣
@@ -18,5 +23,7 @@ public class StringUpdate {
         updateStringBuilder(y);
         System.out.println(x);//第11行的
         System.out.println(y);
+        x = updateString2(x);
+        System.out.println(x);
     }
 }
