54c949a69984eb1e3488d4a81c37123c03434d66
user <user@LAPTOP-QR26C5JR>
2021-03-05 19:59:17 +0800

    Java OCP

--- src/main/java/com/ocp/day06/StringUpdate.java
+++ src/main/java/com/ocp/day06/StringUpdate.java
@@ -5,13 +5,18 @@ public class StringUpdate {
        x = x.concat("8");
     }
     
-    public static void updateStringBuilder(StringBuilder y) {
-        y = y.append("8");
+    public static void updateStringBuilder(StringBuilder y) {//(StringBuilder y)區域變數
+        //y = y.append("8");
+        y.append("8");//與上面答案一樣
     }
     
     public static void main(String[] args) {
         String x = "Java";
         StringBuilder y = new StringBuilder("Java");
         
+        updateString(x);//只有執行沒有接續，所以就被刪除了
+        updateStringBuilder(y);
+        System.out.println(x);//第11行的
+        System.out.println(y);
     }
 }
