f70e8e7e64109455366ecb9c6cc7bfbf71715b08
user <user@LAPTOP-QR26C5JR>
2021-03-12 19:50:35 +0800

    Java OCP

--- src/main/java/com/ocp/day08/Employees.java
+++ src/main/java/com/ocp/day08/Employees.java
@@ -5,6 +5,19 @@ import java.util.stream.Stream;
 public class Employees {
     private String[] employees = {"John","Marry","Helen"};
     
+    //SingleTon(設計模式)
+    //自行建立一個 Static Employees 物件(實體)
+    private static Employees _instance = new Employees();
+    //設定 private 建構子
+    private Employees(){
+
+    }
+    //提供一個 public static方法讓外界得到ˋ你建立的唯一物件(實例)
+    public static Employees getEmployees(){
+        return _instance;
+    }
+        
+    
     public void print() {
         Stream.of(employees).forEach(System.out::println);
         //Stream.of(employees).forEach(e -> System.out.println(e));
--- src/main/java/com/ocp/day08/QueryEmployees.java
+++ src/main/java/com/ocp/day08/QueryEmployees.java
@@ -4,5 +4,7 @@ public class QueryEmployees {
     public static void main(String[] args) {
         Employees e = new Employees();
         e.print();
+        Employees e2 = new Employees();
+        e2.print();
     }
 }
