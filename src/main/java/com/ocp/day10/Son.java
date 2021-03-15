b841e325f96767be5a8cedfc9a1b084d51922886
user <user@LAPTOP-QR26C5JR>
2021-03-15 19:37:09 +0800

    Java OCP

--- src/main/java/com/ocp/day10/Son.java
+++ src/main/java/com/ocp/day10/Son.java
@@ -1,5 +1,11 @@
 package com.ocp.day10;
 
 public class Son extends Father{
+
+    @Override
+    public void hobby() {
+        super.hobby();//父類別 = super class，呼叫super的hobby
+        System.out.println("LOL");//修改成自己的hobby
+    }
     
 }
