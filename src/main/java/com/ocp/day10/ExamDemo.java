e2fb94a919de4f07cc4003b7a4d4eea95f0818e6
user <user@LAPTOP-QR26C5JR>
2021-03-15 20:05:07 +0800

    Java OCP

--- src/main/java/com/ocp/day10/ExamDemo.java
+++ src/main/java/com/ocp/day10/ExamDemo.java
@@ -5,8 +5,8 @@ class Bar {
     Bar() {
         System.out.println("A");
     }
-    Bar(int x){
-        this();
+    Bar(int x) {
+        this();//呼叫自己的建構子(Bar(){})
         System.out.println("B");
     }
 }
@@ -14,11 +14,11 @@ class Bar {
 class Foo extends Bar {
 
     Foo() {
-        this(10);//this()呼叫自己的建構子(Foo(int x){})
+        this(10);//this()呼叫自己有int的建構子(Foo(int x){})
         System.out.println("C");
     }
     Foo(int x) {//Foo的建構子
-        super(20);
+        super(20);//呼叫父類別有int的建構子(Bar(int x){})
         System.out.println("D");
     }
 
