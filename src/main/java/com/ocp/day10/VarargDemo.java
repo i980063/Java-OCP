c66dde35576a7284dca0e87d3d315929ee0d93d3
user <user@LAPTOP-QR26C5JR>
2021-03-15 19:15:59 +0800

    Java OCP

--- src/main/java/com/ocp/day10/VarargDemo.java
+++ src/main/java/com/ocp/day10/VarargDemo.java
@@ -4,6 +4,15 @@ import java.util.stream.IntStream;
 
 public class VarargDemo {
     public static void main(String[] args) {
+        //A
+        if(true){
+            System.out.println("Hello World!!");
+        }
+        //B
+        System.out.println("Hello World!!");
+        
+        System.out.println(10+20+30);
+        System.out.println(60);
         System.out.println(add(10, 20));
         System.out.println(add(10, 20, 30));
         int[] vlaues ={10,20,30,40,50};
@@ -12,12 +21,12 @@ public class VarargDemo {
         System.out.println(add(10,20,30,40));
         System.out.println(add());
     }
-    public static int add(int x , int y) {
-        return x + y ;
-    }
-    public static int add(int x, int y , int z) {
-        return x + y + z;
-    }
+    //public static int add(int x , int y) {
+        //return x + y ;
+    //}
+    //public static int add(int x, int y , int z) {
+        //return x + y + z;
+    //}
     //public static int add(int[] values) {
         //return IntStream.of(values).sum();
     //}
