0f0ee8bdfb4ff3e41acc37de2a34e01d03387188
user <user@LAPTOP-QR26C5JR>
2021-03-15 19:53:24 +0800

    Java OCP

--- src/main/java/com/ocp/day10/Family.java
+++ src/main/java/com/ocp/day10/Family.java
@@ -2,7 +2,8 @@ package com.ocp.day10;
 
 public class Family {
     public static void main(String[] args) {
-        Son son = new Son();
+        Son son = new Son();//一個物件，但是內涵父類別物件
+        /*先建立父類別的物件在建立兒子類別的物件*/
         son.hobby();
     }
 }
--- src/main/java/com/ocp/day10/Son.java
+++ src/main/java/com/ocp/day10/Son.java
@@ -2,6 +2,10 @@ package com.ocp.day10;
 
 public class Son extends Father{
 
+    public Son() {
+        System.out.println("兒子被建立");
+    }
+    
     @Override
     public void hobby() {
         super.hobby();//父類別 = super class，呼叫super的hobby
