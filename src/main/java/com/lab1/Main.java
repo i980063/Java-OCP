7b404bb69f1d44cf382e49ca6e04c2c363965d3e
user <user@LAPTOP-QR26C5JR>
2021-03-19 21:28:15 +0800

    Java OCP

--- src/main/java/com/lab1/Main.java
+++ src/main/java/com/lab1/Main.java
@@ -15,8 +15,5 @@ public class Main {
         int sum = util.getAllSalary(e1, e2, e3, e4, e5, e6);
         System.out.println(sum);
         
-        int sum2 = util.getAllSalary(e4 ,e5);
-        System.out.println(sum2);
-        
     }
 }
