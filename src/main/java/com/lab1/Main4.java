877661553c96537fc7a1b2339ea4d8c6fb5d7e31
user <user@LAPTOP-QR26C5JR>
2021-03-19 21:35:50 +0800

    Java OCP

---
+++ src/main/java/com/lab1/Main4.java
@@ -0,0 +1,20 @@
+package com.lab1;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Employee employee = new Employee(38000);
+        
+        Manager manager = new Manager(68000);
+        manager.setBudget(30000);
+        
+        Director director = new Director(88000);
+        director.setBudget(50000);
+        director.setStockOptions(10_0000);
+        
+        EmployeeUtil util = new EmployeeUtil();
+        
+        System.out.println(util.getAllSalary(employee ,manager ,director));
+        System.out.println(util.getAllBudget(manager ,director));
+        System.out.println(util.getAllStockOptions(director));
+    }
+}
