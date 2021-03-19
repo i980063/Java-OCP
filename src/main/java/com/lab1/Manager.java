7428a31ed760df11e4c74569ef02c7cc5f3832cd
user <user@LAPTOP-QR26C5JR>
2021-03-19 20:33:25 +0800

    Java OCP

---
+++ src/main/java/com/lab1/Manager.java
@@ -0,0 +1,21 @@
+package com.lab1;
+
+
+public class Manager extends Employee {
+
+    private int budgert;
+
+    public Manager() {
+    }
+
+    public Manager(int salary) {
+    }
+
+    public int getBudget() {
+        return 0;
+    }
+
+    public void setBudget(int budget) {
+    }
+
+}
