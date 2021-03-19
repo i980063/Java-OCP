50e96d24b9421f236cbb35c696d26dbf607c1f73
user <user@LAPTOP-QR26C5JR>
2021-03-19 20:56:38 +0800

    Java OCP

--- src/main/java/com/lab1/EmployeeUtil.java
+++ src/main/java/com/lab1/EmployeeUtil.java
@@ -5,7 +5,7 @@ package com.lab1;
 import java.util.stream.Stream;
 
 public class EmployeeUtil {
-    //取得所有員工的薪資
+    //取得所有員工(Employee包含Managers、Dirctors)的薪資
     public int getAllSalary(Employee...employees){
         //JAVA7 
 //        int sum = 0 ;
@@ -18,4 +18,15 @@ public class EmployeeUtil {
         //return Stream.of(employees).mapToInt(e -> e.getSalary()).sum();
         return Stream.of(employees).mapToInt(Employee::getSalary).sum();
     }
+    
+    //取得所有經理(Manager)的薪資
+    public int getAllSalary(Manager...managers){
+        return  Stream.of(managers).mapToInt(Manager::getSalary).sum();
+    }
+    
+    //取得所有董事(Director)的薪資
+    public int getAllSalary(Director...directors){
+        return Stream.of(directors).mapToInt(Director::getSalary).sum();
+    }
+    
 }
---
+++ src/main/java/com/lab1/Main.java
@@ -0,0 +1,19 @@
+package com.lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        //員工
+        Employee e1 = new Employee(38000);
+        Employee e2 = new Employee(48000);
+        Employee e3 = new Employee(58000);
+        //經理
+        Employee e4 = new Manager(68000);
+        Employee e5 = new Manager(78000);
+        //董事
+        Employee e6 = new Director(88000);
+        EmployeeUtil util = new EmployeeUtil();
+        int sum = util.getAllSalary(e1, e2, e3, e4, e5, e6);
+        System.out.println(sum);
+        
+    }
+}
