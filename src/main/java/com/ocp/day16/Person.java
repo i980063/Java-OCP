0942b2bc56d79c37b8879f8872898731f4d589e8
user <user@LAPTOP-QR26C5JR>
2021-04-09 19:08:19 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day16/Person.java
@@ -0,0 +1,16 @@
+package com.ocp.day16;
+
+public class Person {
+    private String name;
+    private Double h;
+    private Double w;
+
+    public Person() {
+    }
+
+    public Person(String name, Double h, Double w) {
+        this.name = name;
+        this.h = h;
+        this.w = w;
+    } 
+}
---
+++ src/main/java/com/ocp/day16/PersonMain.java
@@ -0,0 +1,7 @@
+package com.ocp.day16;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person p1 = new Person("John", 170.0, 60.0);
+    }
+}
