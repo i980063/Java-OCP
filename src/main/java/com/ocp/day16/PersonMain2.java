8c547dcff64e4c80f604622c7c02717b39582652
user <user@LAPTOP-QR26C5JR>
2021-04-09 21:17:56 +0800

    Java OCP

--- src/main/java/com/ocp/day16/PersonMain2.java
+++ src/main/java/com/ocp/day16/PersonMain2.java
@@ -3,11 +3,13 @@ package com.ocp.day16;
 public class PersonMain2 {
     public static void main(String[] args) {
         Person[][] people={
-            {new Person("John", 170.0, 60.0),
+            {
+             new Person("John", 170.0, 60.0),
              new Person("Mary", 150.0, 50.0),
              new Person("Helen", 165.0, 80.0)
             },//A班
-            {new Person("Bob", 180.0, 80.0),
+            {
+             new Person("Bob", 180.0, 80.0),
              new Person("Jo", 168.0, 58.0),
             },//B班
         };
