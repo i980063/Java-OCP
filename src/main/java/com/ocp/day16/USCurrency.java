d6542111dc5435795b5d9d745009d8d3eab7115b
user <user@LAPTOP-QR26C5JR>
2021-04-09 21:12:05 +0800

    Java OCP

--- src/main/java/com/ocp/day16/USCurrency.java
+++ src/main/java/com/ocp/day16/USCurrency.java
@@ -3,6 +3,7 @@ package com.ocp.day16;
 //PENNY = 1美分 , NICLE = 5美分 , DIME = 10美分 , QUARTER = 25美分
 public enum USCurrency {
     PENNY(1),NICLE(5),DIME(20),QUARTER(25);
+    
     private int value;
     
     private USCurrency(int value){
