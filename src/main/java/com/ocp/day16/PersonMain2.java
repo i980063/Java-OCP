e09756d41ce5fb9dd03afc43ca4a4ece8863ace6
user <user@LAPTOP-QR26C5JR>
2021-04-09 21:17:29 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day16/PersonMain2.java
@@ -0,0 +1,16 @@
+package com.ocp.day16;
+
+public class PersonMain2 {
+    public static void main(String[] args) {
+        Person[][] people={
+            {new Person("John", 170.0, 60.0),
+             new Person("Mary", 150.0, 50.0),
+             new Person("Helen", 165.0, 80.0)
+            },//A班
+            {new Person("Bob", 180.0, 80.0),
+             new Person("Jo", 168.0, 58.0),
+            },//B班
+        };
+        //請印出此5人的BMI資料
+    }
+}
