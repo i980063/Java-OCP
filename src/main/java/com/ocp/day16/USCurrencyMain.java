9c94291173b9e431c3d2ce69d77c1f99ab7d2906
user <user@LAPTOP-QR26C5JR>
2021-04-09 21:10:39 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day16/USCurrencyMain.java
@@ -0,0 +1,8 @@
+package com.ocp.day16;
+
+public class USCurrencyMain {
+    public static void main(String[] args) {
+        USCurrency uSCoin = USCurrency.NICLE;
+        System.out.println(uSCoin.getValue());
+    }
+}
