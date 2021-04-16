03dd1cbb0a72881634ededf949acce9aa53372c2
user <user@LAPTOP-QR26C5JR>
2021-04-16 20:33:33 +0800

    Java OCP

--- src/main/java/com/ocp/day18/ArrayListDemo3.java
+++ src/main/java/com/ocp/day18/ArrayListDemo3.java
@@ -2,14 +2,23 @@ package com.ocp.day18;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ArrayListDemo3 {
     public static void main(String[] args) {
         List<Integer> lotto = new ArrayList<>();
-          lotto.add(2); // 0
-          lotto.add(1); // 1
-          lotto.add(4); // 2
-          lotto.add(3); // 3
-          lotto.add(5); // 4
+        lotto.add(2); // 0
+        lotto.add(1); // 1
+        lotto.add(4); // 2
+        lotto.add(3); // 3
+        lotto.add(5); // 4
+        System.out.println(lotto);
+        ListIterator<Integer> iter = lotto.listIterator();
+        while (iter.hasNext()) {
+            int idx = iter.nextIndex();
+            int value = iter.next();
+            System.out.printf("[%d] : %d\n",idx,value);
+        }
+        
     }
 }
