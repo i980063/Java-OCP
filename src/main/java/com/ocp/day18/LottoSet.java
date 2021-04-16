103e8c32d6d6cf106287338f16fbdd5336167fa6
user <user@LAPTOP-QR26C5JR>
2021-04-16 19:11:14 +0800

    Java OCP

--- src/main/java/com/ocp/day18/LottoSet.java
+++ src/main/java/com/ocp/day18/LottoSet.java
@@ -9,7 +9,7 @@ public class LottoSet {
         //0~9 取出5個不重複的數字，並放到 LinkedHashSet 集合存放
         Random r = new Random();
         Set<Integer> lotto = new LinkedHashSet<>();
-        for(int i = 0 ; i<5 ;i++){
+        for(int i = 0 ; i<7 ;i++){
             int n = r.nextInt(39);
             System.out.printf("n = %d\n", n);
             lotto.add(n);//加入到集合
@@ -18,7 +18,7 @@ public class LottoSet {
         System.out.println(lotto.size());
         //使用while
         Set<Integer> lotto2 = new LinkedHashSet<>();
-        while (lotto2.size() < 5) {            
+        while (lotto2.size() < 7) {            
             int n = r.nextInt(39);
             System.out.printf("n = %d\n", n);
             lotto2.add(n);//加入到集合
