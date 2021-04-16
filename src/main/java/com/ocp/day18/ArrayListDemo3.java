8edd014df5819e9b6f323a1cd158960466bbc834
user <user@LAPTOP-QR26C5JR>
2021-04-16 20:50:54 +0800

    Java OCP

--- src/main/java/com/ocp/day18/ArrayListDemo3.java
+++ src/main/java/com/ocp/day18/ArrayListDemo3.java
@@ -1,8 +1,10 @@
 package com.ocp.day18;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Random;
+import java.util.Set;
 
 public class ArrayListDemo3 {
     public static void main(String[] args) {
@@ -13,12 +15,15 @@ public class ArrayListDemo3 {
         lotto.add(3); // 3
         lotto.add(5); // 4
         System.out.println(lotto);
-        ListIterator<Integer> iter = lotto.listIterator();
-        while (iter.hasNext()) {
-            int idx = iter.nextIndex();
-            int value = iter.next();
-            System.out.printf("[%d] : %d\n",idx,value);
+        Set<Integer> ans = new LinkedHashSet<>();
+        Random r = new Random();
+        for(int i = 0; i<3; i++){
+            int idx = r.nextInt(lotto.size());
+            int value = lotto.get(idx);
+            ans.add(value);
+            lotto.remove(idx);
         }
-        
+        System.out.println(lotto);
+        System.out.println(ans);
     }
 }
