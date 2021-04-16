5f990f61c1b6d4e41dcc465457464651415d0554
user <user@LAPTOP-QR26C5JR>
2021-04-16 20:26:50 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day18/ArrayListDemo3.java
@@ -0,0 +1,15 @@
+package com.ocp.day18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo3 {
+    public static void main(String[] args) {
+        List<Integer> lotto = new ArrayList<>();
+          lotto.add(2); // 0
+          lotto.add(1); // 1
+          lotto.add(4); // 2
+          lotto.add(3); // 3
+          lotto.add(5); // 4
+    }
+}
