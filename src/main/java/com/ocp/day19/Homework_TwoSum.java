f7978590340decd2b099f9b2d7e1b2d6af32687f
user <user@LAPTOP-QR26C5JR>
2021-04-19 21:39:39 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day19/Homework_TwoSum.java
@@ -0,0 +1,22 @@
+
+package com.ocp.day19;
+
+import java.util.List;
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+Given nums = [2, 7, 11, 15], target = 9, 這個List李相加=9的兩個組合
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1] 要回傳List
+*/
+public class Homework_TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> twoSum(List<Integer> nums, int target) {
+        return null;
+    }
+}
