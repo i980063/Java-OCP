ca39a8d3de7a96f124c0ee0229d8b1402d2445ff
user <user@LAPTOP-QR26C5JR>
2021-04-19 21:30:25 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day19/LinkedHashMapDemo2.java
@@ -0,0 +1,19 @@
+package com.ocp.day19;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class LinkedHashMapDemo2 {
+    public static void main(String[] args) {
+        Map<String, Integer> exams = new LinkedHashMap<>();
+        exams.put("國文", 100);
+        exams.put("英文", 90);
+        exams.put("數學", 80);
+        //求總分
+        exams.entrySet().stream().forEach(System.out::println);
+        int sum = exams.entrySet().stream().mapToInt(entry -> entry.getValue()).sum();
+        System.out.println(sum);
+    }
+}
