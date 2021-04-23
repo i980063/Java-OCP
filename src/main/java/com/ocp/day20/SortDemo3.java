22dfd225baea120fc223e30efe88501e3fc2f375
user <user@LAPTOP-QR26C5JR>
2021-04-23 19:50:37 +0800

    Java OCP

--- src/main/java/com/ocp/day20/SortDemo3.java
+++ src/main/java/com/ocp/day20/SortDemo3.java
@@ -2,6 +2,7 @@ package com.ocp.day20;
 
 import java.util.Arrays;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 public class SortDemo3 {
     public static void main(String[] args) {
@@ -13,5 +14,9 @@ public class SortDemo3 {
                 new User("E", 90));
         System.out.println(users);
         //請按照分數大小來排序並置入一個新集合中
+        List<User> sortedUsers = users.stream()
+                .sorted((u1, u2) -> u2.getScore() - u1.getScore())
+                .collect(toList());
+        System.out.println(sortedUsers);
     }
 }
