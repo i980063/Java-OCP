e270f136f2e3c5c75b8b5dc72d0631980bef2f79
user <user@LAPTOP-QR26C5JR>
2021-04-23 19:20:00 +0800

    Java OCP

--- src/main/java/com/ocp/day20/SortDemo1.java
+++ src/main/java/com/ocp/day20/SortDemo1.java
@@ -9,16 +9,19 @@ public class SortDemo1 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(100, 80, 90);// unrditable list
         System.out.println(list);
-        //排序(自然排序:小到大)
+        //排序(自然排序:小->大)
         Collections.sort(list);
         System.out.println(list);
-        //排序(大到小)
+        //排序(大->小)
         Collections.sort(list, new Comparator<Integer>(){
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o2 - o1;
             }
         });
+        System.out.println(list);
+        //排序(小->大)(Lmabda表達式)
+        Collections.sort(list, (o1, o2) -> o1 - o2);
         System.out.println(list);
     }
 }
