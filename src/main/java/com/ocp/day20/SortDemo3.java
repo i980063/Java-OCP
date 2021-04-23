332e58dbb319181bce6afbdf239e6b34efc8c3ec
user <user@LAPTOP-QR26C5JR>
2021-04-23 19:55:58 +0800

    Java OCP

--- src/main/java/com/ocp/day20/SortDemo3.java
+++ src/main/java/com/ocp/day20/SortDemo3.java
@@ -1,6 +1,7 @@
 package com.ocp.day20;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 
@@ -14,9 +15,15 @@ public class SortDemo3 {
                 new User("E", 90));
         System.out.println(users);
         //請按照分數大小來排序並置入一個新集合中
-        List<User> sortedUsers = users.stream()
+        List<User> reversedUsers = users.stream()
                 .sorted((u1, u2) -> u2.getScore() - u1.getScore())
                 .collect(toList());
-        System.out.println(sortedUsers);
+        System.out.println(reversedUsers);
+        
+        //請按照分數小大來排序並置入一個新集合中
+        List<User> SortedUsers = users.stream()
+                .sorted(Comparator.comparingInt(u -> u.getScore()))
+                .collect(toList());
+        System.out.println(SortedUsers);
     }
 }
