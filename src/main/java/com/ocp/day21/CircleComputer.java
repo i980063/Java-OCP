77cb4e4ffff973a0c1efce6c1f777a873136a219
user <user@LAPTOP-QR26C5JR>
2021-04-26 20:19:26 +0800

    Java OCP

--- src/main/java/com/ocp/day21/CircleComputer.java
+++ src/main/java/com/ocp/day21/CircleComputer.java
@@ -3,11 +3,20 @@ package com.ocp.day21;
 import java.util.Scanner;
 
 public class CircleComputer {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("請輸入半徑:");
+
+        try {
         double r = sc.nextDouble();
-        double area = Math.pow(r, 2)*Math.PI;
-        System.out.printf("area: %.2f\n",area);
+        double area = Math.pow(r, 2) * Math.PI;
+        System.out.printf("area: %.2f\n", area);
+        } catch (Exception e) {
+            System.out.println("輸入錯誤");
+            System.out.println("錯誤原因" + e);
+        }
+
+
     }
 }
