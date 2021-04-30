44ce4672a237239193e4935bc5f0d950f213eea7
user <user@LAPTOP-QR26C5JR>
2021-04-30 21:29:29 +0800

    Java OCP

--- src/test/java/com/ocp/test23/TestPower.java
+++ src/test/java/com/ocp/test23/TestPower.java
@@ -8,11 +8,6 @@ public class TestPower {
 
     @Test// 測試 W
     public void testW() {
-        // 測試要符合 3A 的條件
-        // Arrange - 安排(目標物件)
-        // Act - 目標方法
-        // Assert - 驗證是否符合預期
-
         // Arrange - 安排(目標物件)
         Power square = new Power();
 
@@ -25,6 +20,44 @@ public class TestPower {
 
         // Act - 目標方法
         int act = square.getW(v, a);// Act 表示實際計算的值
+
+        // Arrange - 安排(目標物件)
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test// 測試 V
+    public void testV() {
+        // Arrange - 安排(目標物件)
+        Power square = new Power();
+
+        //參數配置
+        int W = 1000;
+        int a = 100;
+
+        //期望結果
+        int exp =10;
+
+        // Act - 目標方法
+        int act = square.getV(W, a);// Act 表示實際計算的值
+
+        // Arrange - 安排(目標物件)
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test// 測試 A
+    public void testA() {
+        // Arrange - 安排(目標物件)
+        Power square = new Power();
+
+        //參數配置
+        int W = 1000;
+        int v = 10;
+
+        //期望結果
+        int exp =100;
+
+        // Act - 目標方法
+        int act = square.getA(W, v);// Act 表示實際計算的值
 
         // Arrange - 安排(目標物件)
         Assert.assertEquals(exp, act);
