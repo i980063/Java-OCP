c9a972a12a28d3bca285bfd8b1af8b9e55d21cd4
user <user@LAPTOP-QR26C5JR>
2021-05-03 21:04:59 +0800

    Java OCP

--- src/main/java/com/ocp/day24/RaceMain.java
+++ src/main/java/com/ocp/day24/RaceMain.java
@@ -4,9 +4,10 @@ public class RaceMain {
     public static void main(String[] args) {
         Race race = new Race();
         Race race2 = new Race();
+        //設定執行緒名稱
         race.setName("烏龜");
         race2.setName("兔子");
-        
+        //執行緒啟動
         race.start();
         race2.start();
     }
