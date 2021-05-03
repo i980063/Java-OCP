1752ab9ea9a5861ab5b539028b1f3fac39afa942
user <user@LAPTOP-QR26C5JR>
2021-05-03 21:33:16 +0800

    Java OCP

--- src/main/java/com/ocp/day24/Report.java
+++ src/main/java/com/ocp/day24/Report.java
@@ -1,11 +1,11 @@
 package com.ocp.day24;
 
 // 要給執行緒執行的工作任務
-public class Programming implements Runnable{
+public class Report implements Runnable{
     
     @Override
-    public void run(){//寫程式的工作
+    public void run(){//寫報表的工作
         String tname = Thread.currentThread().getName();
-        System.out.println("寫 JAVA 程式");
+        System.out.println("寫 PPT 報表");
     }
 }
