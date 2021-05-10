55c5469da86b31b01aeca58068d623297a9b763b
user <user@LAPTOP-QR26C5JR>
2021-05-10 19:30:11 +0800

    Java OCP

--- src/main/java/com/ocp/day26/Account.java
+++ src/main/java/com/ocp/day26/Account.java
@@ -19,7 +19,22 @@ public class Account {
         String tname = Thread.currentThread().getName();
         System.out.printf("%s 準備提款 $%d\n", tname, amount);
         // 檢查帳戶餘額
-        
+        // 1.先取得目前的帳戶餘額
+        int currentBalance = getBalance();
+        // 2.目前帳戶餘額是否足夠提款
+        if(currentBalance >= amount) {
+            // 開始提款
+            // 利用 for-loop 模擬提款所花的時間
+            for(int i=0 ; i<Integer.MAX_VALUE ; i++);
+            // 變更帳戶餘額
+            // 帳戶餘額 = 目前帳戶餘額 - 提款金額
+            balance = currentBalance - amount;
+            // 印出交易成功清單
+            System.out.printf("%s 提款 $%d 成功, 帳戶餘額 $%d\n", tname, amount, getBalance());
+        } else {
+            // 印出交易失敗清單
+            System.out.printf("%s 提款 $%d 失敗(餘額不足), 帳戶餘額 $%d\n", tname, amount, getBalance());
+        }
         
     }
     
---
+++ src/main/java/com/ocp/day26/WithdrawThread.java
@@ -0,0 +1,15 @@
+package com.ocp.day26;
+
+public class WithdrawThread extends Thread{
+    private Account account;
+    private int amount;
+    public WithdrawThread(Account account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+    
+    @Override
+     public void run(){
+         account.withdraw(5000);
+     }
+}
